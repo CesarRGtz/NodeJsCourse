4bba831b488a9c130e7fca018cadde6e0454080b
ZrMid <cesarjesusr18062003@gmail.com>
2025-05-15 14:22:56 -0700

    1:06:00

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('hola');
+
+const olaadas = (jamon, edu) => {console.log("lorefasdfklj ls jflasdkfj lksj fljslf jsldfj lj fls jsaljsklklsj lfkjsldfkj slkd aflkjalksd fjklsd jlkas jdfklj sdklfj klsdj fklsadjf");
+}
---
+++ module/myModule.js
@@ -0,0 +1,19 @@
+const myWebAdress = 'https://example.com';
+const myNumber = 42;
+const myString = 'Hello, World!';
+const myBoolean = true;
+const myArray = [1, 2, 3, 4, 5];
+const myObject = {
+    name: 'John Doe',
+    age: 30,
+    city: 'New York',
+};
+
+module.exports = {
+    myWebAdress,
+    myNumber,
+    myString,
+    myBoolean,
+    myArray,
+    myObject,
+};
